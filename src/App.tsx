1487ba69e9180276a3e9dbd03ca05637f2064e51
rakshagirl <68088408+rakshagirl@users.noreply.github.com>
2024-06-04 17:13:40 -0500

    added list group

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import Message from "./Message";
+import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><Message></Message></div>;
+
+  let items = [
+    'Chennai',
+    'Madurai',
+    'Salem',
+    'Erode',
+    'Pondicherry'
+  ];
+
+  return (
+    <>
+      <ListGroup items={items} heading="Cities" />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+interface Props {
+  items: string[],
+  heading: string
+}
+
+function ListGroup({items, heading}: Props) {
+
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+
+    const getMessage = () => {
+        return items.length === 0 && <p>No item found</p>;
+    }
+
+    return (
+        <>
+            <h1>{heading}</h1>
+            {getMessage()}
+            <ul className="list-group">
+                {items.map((item, index) => <li className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'} key={item} onClick={() => {setSelectedIndex(index);}}> {item} </li>)}
+            </ul>
+        </>
+    );
+}
+
+export default ListGroup;
